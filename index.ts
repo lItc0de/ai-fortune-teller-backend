7e2bb524d45e964e01e301e79e12ef884131e5f3
litc0de <litc0de@posteo.org>
2023-12-13 18:25:43 +0100

    fix code

--- index.ts
+++ index.ts
@@ -4,16 +4,16 @@ import Bot from "./bot";
 
 const bot = new Bot();
 
-const sendMsg = async (ws: ServerWebSocket<{}>) => {
-  const res = await bot.chat("Hello how are you?");
-  ws.publish("chat", res);
+const sendMsg = async (ws: ServerWebSocket<{}>, message: string) => {
+  const res = await bot.chat(message);
+  // ws.publish("chat", res);
   ws.send(res);
   console.log("chat:", res);
 };
 
 const server = Bun.serve<{}>({
   fetch(req, server) {
-    const cookies = req.headers.get("cookie");
+    // const cookies = req.headers.get("cookie");
     const success = server.upgrade(req, { data: {} });
     if (success) return undefined;
 
@@ -23,11 +23,10 @@ const server = Bun.serve<{}>({
     open(ws) {
       ws.subscribe("chat");
       console.log("user connected");
-      sendMsg(ws);
     },
     message(ws, message) {
-      // the server re-broadcasts incoming messages to everyone
-      ws.publish("chat", `User1: ${message}`);
+      if (typeof message !== "string") return;
+      sendMsg(ws, message);
     },
     close(ws) {
       const msg = `User1 has left the chat`;
