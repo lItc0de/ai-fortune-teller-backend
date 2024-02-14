9e965b23cf98a8affe3f63b8ae57f10e8480d224
litc0de <litc0de@posteo.org>
2024-02-14 15:24:20 +0100

    update tts

--- tts.ts
+++ tts.ts
@@ -1,5 +1,3 @@
-import { spawnSync } from "child_process";
-
 const PYTHON_PATH = "/Users/basti/miniforge3/envs/fortuneTeller/bin/python";
 const SCRIPT_PATH = "./tts.py";
 
@@ -8,8 +6,19 @@ class TTS {
     // const audioPath = `tts_audios/${Date.now()}.wav`;
     const audioPath = `tts_audios/audio.wav`;
     if (!text) return "";
-    await spawnSync(PYTHON_PATH, [SCRIPT_PATH, text, audioPath, "en_0"]);
+    // await spawnSync(PYTHON_PATH, [SCRIPT_PATH, text, audioPath, "en_0"]);
     // const result = process.stdout?.toString();
+    const proc = Bun.spawn(
+      [PYTHON_PATH, SCRIPT_PATH, text, audioPath, "en_0"],
+      {
+        onExit(proc, exitCode, signalCode, error) {
+          console.log({ proc, exitCode, signalCode, error });
+        },
+      }
+    );
+
+    await proc.exited;
+
     return audioPath;
   };
 }
